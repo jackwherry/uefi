b0b8b542e7d169b83baf5465a4d3b97da7df1474
Jack Wherry <jack@jackwherry.com>
2025-12-31 11:38:29 -0600

    call separated mains for snake and raycast

--- main.c
+++ main.c
@@ -1,16 +1,36 @@
 #include "efi.h"
 
-// EFI_ERROR definitions, lifted from EDK2 headers
-// TODO: rewrite this less obtusely
 // "A value of native width with the highest bit set." -- MdePkg/Include/X64/ProcessorBind.h
 #define MAX_BIT  0x8000000000000000ULL
-typedef EFI_STATUS RETURN_STATUS; // this was backwards in the EDK2 headers but I figured they're the same
-#define RETURN_ERROR(a) (((RETURN_STATUS)(a)) >= MAX_BIT)
-#define EFI_ERROR(A) RETURN_ERROR(A)
 
-CHAR16 *hello_str = L"UEFI Games v0.1.0\r\nbuild 5\r\n\r\n\r\nPress 1 for Snake or 2 for 3D engine\r\n";
+// substantially simplified from the version in the EDK2 headers
+#define EFI_ERROR(a) (((EFI_STATUS)(a)) >= MAX_BIT)
+
 EFI_HANDLE ImageHandle;
 EFI_SYSTEM_TABLE *SystemTable;
+
+EFI_STATUS raycast_main() {
+	EFI_STATUS status;
+	status = SystemTable->ConOut->OutputString(SystemTable->ConOut, L"raycast_main() entered\r\n");
+	if (EFI_ERROR(status)) {
+		return status;
+	}
+	return EFI_SUCCESS;
+}
+
+EFI_STATUS snake_main() {
+	// loop
+	// clear screen, draw box, draw snake position
+	// update snake position, check collision
+	// get input
+	// wait a second
+	EFI_STATUS status;
+	status = SystemTable->ConOut->OutputString(SystemTable->ConOut, L"snake_main() entered\r\n");
+	if (EFI_ERROR(status)) {
+		return status;
+	}
+	return EFI_SUCCESS;
+}
 
 EFI_STATUS efi_main(EFI_HANDLE ih, EFI_SYSTEM_TABLE *st) {
 	ImageHandle = ih;
@@ -18,7 +38,8 @@ EFI_STATUS efi_main(EFI_HANDLE ih, EFI_SYSTEM_TABLE *st) {
 
 	EFI_STATUS status;
 
-	status = SystemTable->ConOut->OutputString(SystemTable->ConOut, hello_str);
+	status = SystemTable->ConOut->OutputString(SystemTable->ConOut, 
+		L"\r\nUEFI Games v0.1.0\r\nbuild 10\r\n\r\n\r\nPress 1 for Snake or 2 for 3D engine\r\n");
 	if (EFI_ERROR(status)) {
 		return status;
 	}
@@ -33,6 +54,12 @@ EFI_STATUS efi_main(EFI_HANDLE ih, EFI_SYSTEM_TABLE *st) {
 	UINTN index;
 	SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &index);
 	SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key); // read to clear state
+
+	if (Key.UnicodeChar == L'1') {
+		status = snake_main();
+	} else if (Key.UnicodeChar == L'2') {
+		status = raycast_main();
+	}
 
 	return status;
 }
