07c582b969b4d68282ddf765127f60f0415126fa
Jack Wherry <jack@jackwherry.com>
2025-12-31 08:45:25 -0600

    waiting for kb input

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include "efi.h"
 
-CHAR16 *hello_str = L"Hello, world!\r\n";
+CHAR16 *hello_str = L"Hello, world! build 4\r\n";
 
 EFI_STATUS efi_main(EFI_HANDLE ih, EFI_SYSTEM_TABLE *st) {
-	st->ConOut->OutputString(st->ConOut, hello_str);
-	while (1) {}
+	st->ConOut->OutputString(st->ConOut, hello_str); // this returns a status but we're just gonna assume it succeeded :)
+
+	// empty console input buffer
+	st->ConIn->Reset(st->ConIn, EFI_FALSE);
+
+	EFI_INPUT_KEY Key;
+	UINTN index;
+	st->BootServices->WaitForEvent(1, &st->ConIn->WaitForKey, &index);
+	st->ConIn->ReadKeyStroke(st->ConIn, &Key); // read to clear state
+
 	return EFI_SUCCESS;
 }
